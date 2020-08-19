b405442650daa92d4b9633f38e3341d07b9b26d4
plbpushpa <69887524+plbpushpa@users.noreply.github.com>
2020-08-19 11:50:46 +0530

    Update Newtest.java

--- src/test/Newtest.java
+++ src/test/Newtest.java
@@ -5,6 +5,7 @@ public class Newtest {
 	public static void main(String[] args) {
 System.out.println("Hello world");
 		System.out.println("Hello selenium");
+		System.out.println("hello java");
 	}
 
 }
